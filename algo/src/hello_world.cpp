b17727bb96988e6bbe56f8fd6a51941805f0165a
Spice <kf.shared.ptr@gmail.com>
2025-10-25 21:05:36 -0400

    Generating bindings for JS using C++/emscripten

--- algo/src/hello_world.cpp
+++ algo/src/hello_world.cpp
@@ -1,7 +1,24 @@
+#ifdef SYSTEM_EMSCRIPTEN
+    #include <emscripten/bind.h>
+#endif
 
+#include <string>
 #include <print>
 
-int main()
+void cpp_printer(const std::string& string)
 {
-    std::println("Hello, world!");
+    std::println("Hello from C++: {}", string);
 }
+
+#ifdef SYSTEM_EMSCRIPTEN
+    using namespace emscripten;
+
+    EMSCRIPTEN_BINDINGS(algo) {
+        function("cpp_printer", &cpp_printer);
+    }
+#else
+    int main()
+    {
+        std::println("Hello, world!");
+    }
+#endif
