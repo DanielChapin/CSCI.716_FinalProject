8c6ee03c76f3e0789b65dfb2b4c9eb740c86e895
Spice <kf.shared.ptr@gmail.com>
2025-12-02 20:29:55 -0500

    Vertex colors for mesh are computed in C++

--- website/src/lib/geometry.ts
+++ website/src/lib/geometry.ts
@@ -12,25 +12,25 @@ export function createGeometry(mesh: Mesh, options: Partial<GeomOptions> = {}): 
     const { genNormals = true } = options;
 
     const geom = new THREE.BufferGeometry();
-    
     const vertices = new Float32Array(mesh.vertices.size() * 3);
     const colors = new Float32Array(mesh.vertices.size() * 3);
     for (let i = 0; i < mesh.vertices.size(); i++) {
         const idx = i * 3;
         const vtx = mesh.vertices.get(i)!;
+        const col = mesh.colors.get(i)!;
         vertices[idx + 0] = vtx.x;
         vertices[idx + 1] = vtx.y;
         vertices[idx + 2] = vtx.z;
-        colors[idx + 0] = 1;
-        colors[idx + 1] = 0;
-        colors[idx + 2] = 1;
+        colors[idx + 0] = col.x;
+        colors[idx + 1] = col.y;
+        colors[idx + 2] = col.z;
     }
     geom.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
     geom.setAttribute('color', new THREE.BufferAttribute(colors, 3));
-    
+
     const idxs = new Uint32Array(mesh.indices.size()).map((_, i) => mesh.indices.get(i)!);
     geom.setIndex(new THREE.BufferAttribute(idxs, 1));
-    
+
     const geomSmooth = BufferGeometryUtils.mergeVertices(geom);
     if (genNormals) geomSmooth.computeVertexNormals();
 
